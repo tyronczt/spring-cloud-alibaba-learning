391080fc7ed540d5804209f5f791c720954745d7
tyronczt <310674619@qq.com>
2021-01-13 21:18:25 +0800

    子项目-nacos-服务注册

---
+++ provider/src/test/java/com/tyron/provider/ProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyron.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
