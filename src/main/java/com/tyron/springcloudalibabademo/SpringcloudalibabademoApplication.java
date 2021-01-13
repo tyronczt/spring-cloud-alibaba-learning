e9073a525f0e935176ad8137107cc905fc059b66
tyronczt <310674619@qq.com>
2021-01-13 21:16:03 +0800

    初始化父项目

---
+++ src/main/java/com/tyron/springcloudalibabademo/SpringcloudalibabademoApplication.java
@@ -0,0 +1,13 @@
+package com.tyron.springcloudalibabademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudalibabademoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudalibabademoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tyron/springcloudalibabademo/SpringcloudalibabademoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyron.springcloudalibabademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudalibabademoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
