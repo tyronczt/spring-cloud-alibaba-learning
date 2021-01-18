bb4a05cac259072adda0969031be32aaf86126e7
tyronczt <310674619@qq.com>
2021-01-18 22:38:07 +0800

    Consumer的RestTemplate配置文件

---
+++ consumer/src/main/java/com/tyron/consumer/configuration/ConsumerConfig.java
@@ -0,0 +1,18 @@
+package com.tyron.consumer.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @Description: 服务调用配置文件
+ * @Author: tyron
+ * @Date: Created in 2021/1/18
+ */
+@Configuration
+public class ConsumerConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
