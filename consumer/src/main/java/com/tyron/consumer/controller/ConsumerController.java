93c2b0c11644384d5ba269036834b56c8e40221c
tyronczt <310674619@qq.com>
2021-01-18 22:38:42 +0800

    ConsumerController

--- consumer/src/main/java/com/tyron/consumer/controller/ConsumerController.java
+++ consumer/src/main/java/com/tyron/consumer/controller/ConsumerController.java
@@ -5,8 +5,10 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @Description: 消费者控制器
@@ -19,9 +21,20 @@ public class ConsumerController {
     @Autowired
     private DiscoveryClient discoveryClient;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/instances")
-    private List<ServiceInstance> instances(){
+    private List<ServiceInstance> instances() {
         return this.discoveryClient.getInstances("provider");
+    }
+
+    @GetMapping("/index")
+    public String index() {
+        List<ServiceInstance> provider = discoveryClient.getInstances("provider");
+        int index = ThreadLocalRandom.current().nextInt(provider.size());
+        String url = provider.get(index).getUri() + "/index";
+        return "consumer随机远程调用provier：" + this.restTemplate.getForObject(url, String.class);
     }
 
 }
